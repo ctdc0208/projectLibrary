c5e2676118bf8641a0460945c328269180a47836
nexus0208 <christiandelacruz0208@gmail.com>
2021-04-25 23:14:44 +0800

    add arrays to display book info

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 
 function Book() {
-  constructor(title, author, pages, status){
+  constructor(title, author, pages, status)
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -16,22 +16,32 @@ function addBookToLibrary() {
 }
 
 
-functions showBooksofLibrary() {
-  for (let i == 0; i < myLibrary.length; i++) {
+function showBooksofLibrary() {
+  var bookList = document.querySelector('table');
+  bookList.testContent();
+  for (let i = 0; i < myLibrary.length; i++) {
+    var bookTable = document.createElement('bookTable');
+    bookTable.classList.add('bookInfo');
+    bookList.appendChild(bookTable;
+
     var bookTitle = document.createElement('bookTitle');
     bookTitle.textContent = myLibrary[i].title;
-    bookTitle.appendChild(bookTitle);
+    bookTable.appendChild(bookTitle);
 
     var bookAuthor = document.createElement('bookAuthor');
     bookAuthor.textContent = myLibrary[i].author;
-    bookAuthor.appendChild(bookAuthor);
+    bookTable.appendChild(bookAuthor);
 
     var bookPages = document.createElement('bookPages');
     bookPages.textContent = myLibrary[i].pages;
-    bookPages.appendChild(bookPages);
+    bookTable.appendChild(bookPages);
 
     var bookStatus = document.createElement('bookStatus');
     bookStatus.textContent = myLibrary[i].status;
-    bookStatus.appendChild(bookStatus);
+    bookTable.appendChild(bookStatus);
   }
+}
+
+function newBook() {
+
 }
